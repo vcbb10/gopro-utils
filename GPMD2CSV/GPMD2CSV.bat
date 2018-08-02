9cdfeab55014b7fde04f2c7fc74b0d9484c02cf6
JuanIrache <yrache@gmail.com>
2018-08-02 17:16:58 +0200

    fixed paths

--- GPMD2CSV/GPMD2CSV.bat
+++ GPMD2CSV/GPMD2CSV.bat
@@ -1,16 +1,16 @@
-mkdir %~dp0exports
+mkdir .\exports
 if [%1]==[] goto :eof
 :loop
-%~dp0bin\ffmpeg -i "%~1" > output.txt 2>&1
+.\bin\ffmpeg -i "%~1" > output.txt 2>&1
 for /F "delims=" %%a in ('FINDSTR "gpmd" output.txt') do set line=%%a
 echo "%line%"
 set stream= %line:~12,3%
 echo "%stream%"
-START /WAIT %~dp0bin\ffmpeg -y -i "%~1" -codec copy -map "%stream%" -f rawvideo "%~n1".bin
-START /WAIT %~dp0bin\gpmd2csv -i "%~n1".bin -o exports/"%~n1".csv
-START /WAIT %~dp0bin\gopro2gpx -i "%~n1".bin -o exports/"%~n1".gpx
-START /WAIT %~dp0bin\gopro2json -i "%~n1".bin -o exports/"%~n1".json
-START /WAIT %~dp0bin\gps2kml -i "%~n1".bin -o exports/"%~n1".kml
+START /WAIT .\bin\ffmpeg -y -i "%~1" -codec copy -map "%stream%" -f rawvideo "%~n1".bin
+START /WAIT .\bin\gpmd2csv -i "%~n1".bin -o exports/"%~n1".csv
+START /WAIT .\bin\gopro2gpx -i "%~n1".bin -o exports/"%~n1".gpx
+START /WAIT .\bin\gopro2json -i "%~n1".bin -o exports/"%~n1".json
+START /WAIT .\bin\gps2kml -i "%~n1".bin -o exports/"%~n1".kml
 DEL "%~n1".bin
 DEL output.txt
 shift
