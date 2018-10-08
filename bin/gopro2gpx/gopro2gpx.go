42adfbdf66a46f1098dc54ff6d65a2cd9261806c
JuanIrache <yrache@gmail.com>
2018-10-08 19:52:38 +0200

    Add comment with gps accuracy to GPX

--- bin/gopro2gpx/gopro2gpx.go
+++ bin/gopro2gpx/gopro2gpx.go
@@ -70,7 +70,7 @@ func main() {
 						Elevation: *gpx.NewNullableFloat64(telems[i].Altitude),
 					},
 					Timestamp: time.Unix(telems[i].TS/1000/1000, telems[i].TS%(1000*1000)*1000).UTC(),
-					Comment: "GpsAccuracy: " + strconv.Itoa(int(telems[i].GpsAccuracy)),
+					Comment: "GpsAccuracy: " + strconv.Itoa(int(t_prev.GpsAccuracy.Accuracy)),
 				},
 			)
 		}
