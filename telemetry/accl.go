6948d1e63317575622fb3f13213f13118b1705ba
fu-iq12 <fu@iq12.com>
2018-10-10 15:28:11 +0200

    fusion cameras have 4 bytes per axis (float32)

--- telemetry/accl.go
+++ telemetry/accl.go
@@ -1,6 +1,7 @@
 package telemetry
 
 import (
+	"math"
 	"encoding/binary"
 	"errors"
 )
@@ -13,14 +14,23 @@ type ACCL struct {
 }
 
 func (accl *ACCL) Parse(bytes []byte, scale *SCAL) error {
-	//if 6 != len(bytes) {	//for some reason this was causing errors on Fusion camera. Still to be fully resolved
-	if false {
-		return errors.New("Invalid length ACCL packet")
+	// hero 5/6 have 2 bytes per axis
+	if 6 == len(bytes) {
+		accl.X = float64(int16(binary.BigEndian.Uint16(bytes[0:2]))) / float64(scale.Values[0])
+		accl.Y = float64(int16(binary.BigEndian.Uint16(bytes[2:4]))) / float64(scale.Values[0])
+		accl.Z = float64(int16(binary.BigEndian.Uint16(bytes[4:6]))) / float64(scale.Values[0])
+
+		return nil
 	}
 
-	accl.X = float64(int16(binary.BigEndian.Uint16(bytes[0:2]))) / float64(scale.Values[0])
-	accl.Y = float64(int16(binary.BigEndian.Uint16(bytes[2:4]))) / float64(scale.Values[0])
-	accl.Z = float64(int16(binary.BigEndian.Uint16(bytes[4:6]))) / float64(scale.Values[0])
+	// fusion cameras have 4 bytes per axis (float32)
+	if 12 == len(bytes) {
+		accl.X = float64(math.Float32frombits(binary.BigEndian.Uint32(bytes[0:4]))) / float64(scale.Values[0])
+		accl.Y = float64(math.Float32frombits(binary.BigEndian.Uint32(bytes[4:8]))) / float64(scale.Values[0])
+		accl.Z = float64(math.Float32frombits(binary.BigEndian.Uint32(bytes[8:12]))) / float64(scale.Values[0])
 
-	return nil
+		return nil
+	}
+
+	return errors.New("Invalid length ACCL packet")
 }
